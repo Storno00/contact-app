2c65d15b576bc7a9cabecb6c77f78c04764aa871
Storno00 <benpeter00@gmail.com>
2022-07-11 17:06:12 +0200

    added url-s

--- frontend/src/constants/url.js
+++ frontend/src/constants/url.js
@@ -1,2 +1,4 @@
+export const CLOUDINARY_URL = process.env.CLOUDINARY_URL || 'https://api.cloudinary.com/v1_1/dooqcjpph/image/upload';
+export const CLOUDINARY_UPLOAD_PRESET = process.env.CLOUDINARY_UPLOAD_PRESET_NAME || 'jjhnwxfp';
 export const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:8080';
-export const CLOUDINARY_UPLOAD_PRESET = process.env.CLOUDINARY_UPLOAD_PRESET_NAME;
+export const BACKEND_CONTACT = `${BACKEND_URL}/api/contact`;
