6467e8a432442803ad16c4bdc9eec386f036775d
Storno00 <benpeter00@gmail.com>
2022-07-11 09:18:38 +0200

    modal components are added to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
+import {useState} from "react";
 import './App.scss';
 import Header from './components/Header/Header';
+import Modal from "./components/Modal/Modal";
+import AddContactModal from "./components/AddContactModal/AddContactModal";
 
-function App() {
+const App = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="app">
-      <Header />
+      <Header setShowModal={setShowModal} />
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <AddContactModal setShowModal={setShowModal} />
+      </Modal>
     </div>
   );
-}
+};
 
 export default App;
