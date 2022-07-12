37051808dbf437feec87646cd43a4366e3033a25
Storno00 <benpeter00@gmail.com>
2022-07-12 14:52:45 +0200

    validation middleware added

---
+++ backend/src/middlewares/validation.js
@@ -0,0 +1,11 @@
+const validation = (schema) => async (req, res, next) => {
+  const { body } = req;
+  try {
+    await schema.validate(body);
+    return next();
+  } catch (err) {
+    return res.status(400).json({ message: err.message });
+  }
+};
+
+export default validation;
