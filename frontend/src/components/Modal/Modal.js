7b5639e3bbb80d38b473f706ac95ec54d41ad80a
Storno00 <benpeter00@gmail.com>
2022-07-13 10:52:14 +0200

    modal animation added

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -1,14 +1,32 @@
 import './Modal.scss';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
+import {useTransition, animated} from 'react-spring';
 
-const Modal = ({ showModal, setShowModal, children }) => {
-  if (!showModal) return null;
+const Modal = ({showModal, setShowModal, children}) => {
+
+  const transition = useTransition(showModal, {
+    config: {duration: 200},
+    from: {opacity: 0, scale: 0},
+    enter: {opacity: 1, scale: 1},
+    leave: {opacity: 0, scale: 0},
+  });
+
   return ReactDOM.createPortal(
     <>
-      <div className="overlay" onClick={() => setShowModal(false)}></div>
-      <div className="modal">
-        {children}
-      </div>
+      {transition((style, item) => (
+        item && (
+          <>
+            <animated.div style={{opacity: style.opacity}}>
+              <div className="overlay" onClick={() => setShowModal(false)}/>
+            </animated.div>
+            <div className="modal">
+              <animated.div style={style}>
+                {children}
+              </animated.div>
+            </div>
+          </>
+        )
+      ))}
     </>,
     document.getElementById('portal')
   );
