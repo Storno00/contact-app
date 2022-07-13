44cba00830330b1f83bc190ec8316a74ed6ba6c3
Storno00 <benpeter00@gmail.com>
2022-07-13 19:53:50 +0200

    update urls

--- frontend/src/constants/url.js
+++ frontend/src/constants/url.js
@@ -1,4 +1,4 @@
-export const CLOUDINARY_URL = process.env.CLOUDINARY_URL || 'https://api.cloudinary.com/v1_1/dooqcjpph/image/upload';
-export const CLOUDINARY_UPLOAD_PRESET = process.env.CLOUDINARY_UPLOAD_PRESET_NAME || 'jjhnwxfp';
-export const BACKEND_URL = process.env.BACKEND_URL || 'http://192.168.1.78:8080';
+export const CLOUDINARY_URL = process.env.REACT_APP_CLOUDINARY_URL || 'https://api.cloudinary.com/v1_1/dooqcjpph/image/upload';
+export const CLOUDINARY_UPLOAD_PRESET = process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET_NAME || 'jjhnwxfp';
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080';
 export const BACKEND_CONTACT = `${BACKEND_URL}/api/contact`;
