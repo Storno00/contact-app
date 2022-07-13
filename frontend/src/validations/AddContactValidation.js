dcd390d462642460f8d2bf4ba6cc78bf62b3758a
Storno00 <benpeter00@gmail.com>
2022-07-13 14:06:46 +0200

    frontend validation now allow to add contact without phone number and email

--- frontend/src/validations/AddContactValidation.js
+++ frontend/src/validations/AddContactValidation.js
@@ -5,12 +5,9 @@ const addContactValidation = Yup.object().shape({
     .min(2, 'At least 2 characters are required for name')
     .max(100, 'Too many characters in name (max. 100)')
     .required('You need to add a name'),
-  phoneNumber: Yup.string()
-    .required('You need to add a phone number'),
   email: Yup.string()
     .email('Invalid email format')
-    .max(100, 'Too many characters in email (max. 100)')
-    .required('You need to add an email'),
+    .max(100, 'Too many characters in email (max. 100)'),
 });
 
 export default addContactValidation;
